4f4e8bcb9682e81f69290374bdaa6d76c85fefa9
Monty <montychain@gmail.com>
2022-10-29 00:52:18 +0300

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function AboutPage (){
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
