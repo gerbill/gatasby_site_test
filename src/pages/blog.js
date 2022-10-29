f78c496defd139df03a01f151e70d00642bb0802
gerbill <s@oxton.ru>
2022-10-30 01:35:30 +0300

    blog posts data to blog posts list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,44 @@
-import * as React from 'react'
-import { graphql } from "gatsby";
+import * as React from "react";
+import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-
-export default function BlogPage ({data}) {
-  console.log(data)
+export default function BlogPage({ data }) {
+  console.log(data);
   return (
-    <Layout pageTitle='My Glog Posts'>
-      <ul>
-        {data.allFile.nodes.map(node => <li key={node.name}>{node.name}</li>)}
-      </ul>
+    <Layout pageTitle="My Glog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Link to={node.frontmatter.slug}>
+            <h2>{node.frontmatter.title}</h2>
+          </Link>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
-  )
+  );
 }
 
 export const query = graphql`
   query {
-      allFile {
-          nodes {
-              name
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+        excerpt
+        parent {
+          ... on File {
+            modifiedTime(formatString: "MMMM D, YYYY")
           }
+        }
       }
+    }
   }
-`
+`;
 
-export const Head = () => <Seo title='My Blog Posts' />
+export const Head = () => <Seo title="My Blog Posts" />;
